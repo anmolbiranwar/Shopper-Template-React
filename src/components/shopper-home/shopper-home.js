8988ed07c39f2a986c0059acede147359690e7c4
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-20 13:36:50 -0700

    Update shopper-home.js

--- src/components/shopper-home/shopper-home.js
+++ src/components/shopper-home/shopper-home.js
@@ -2,19 +2,19 @@
 export function ShopperHome()
 {
     return(
-        <div className="container-fluid">
-            <div className="d-flex">
+        <div className="container-fluid mt-5">
+            <div className="d-flex justify-content-between">
                 <div>
-                    <img src="electronics.jpg"/>
+                    <img src="electronics.jpg" style={{width:'250px',height:'350px'}}/>
                 </div>
                 <div>
-                    <img src="men'sclothing.jpg"/>
+                    <img src="men'sclothing.jpg" style={{width:'250px',height:'350px'}}/>
                 </div>
                 <div>
-                    <img src="jewelery.jpg"/>
+                    <img src="jewelery.jpg" style={{width:'250px',height:'350px'}}/>
                 </div>
                 <div>
-                    <img src="women'sclothing.jpg"/>
+                    <img src="women'sclothing.jpg" style={{width:'250px',height:'350px'}}/>
                 </div>
                 
             </div>
